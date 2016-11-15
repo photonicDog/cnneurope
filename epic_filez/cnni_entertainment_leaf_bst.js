5f5f53a47abbf604e0be8bb78a43048a7ef1703b
fen <fen@nice.lgbt>
2016-11-16 00:35:15 +0100

    base copy

---
+++ epic_filez/cnni_entertainment_leaf_bst.js
@@ -0,0 +1,111 @@
+
+AMPTManager.registry.push([
+{
+  "rktr_deployed_date": "2016-04-13 11:26:42",
+  "rktr_slot_id": "page",
+  "rktr_id": "cnni_entertainment_leaf_bst",
+  "gpt_id": "8663477",
+  "site": "cnni",
+  "root": "CNNI",
+  "targeting": [["transId", cnnad_transactionID], ["kuid", Krux.user], ["ksg", Krux.segments], ["guid", AMPTManager.readCookie("ug")]]
+},
+{
+  "rktr_slot_id": "ad_bnr_atf_01",
+  "rktr_ad_id": "CNNi/entertainment/leaf/bst",
+  "sizes": [[1,2],[320,50],[728,90],[970,66],[970,90],[970,250]],
+  "hasInViewRefresh": "false",
+  "inViewRefreshCount": "0",  
+  "targeting": [["pos",["bnr_atf_01"]]],
+  "responsive":[[['1024', '0'], [['970', '250'], ['970', '90'], ['970', '66'], ['728', '90'], ['1', '2']]], [['782', '0'], [['728', '90'], ['1', '2']]], [['0', '0'], [['320', '50'], ['1', '2']]]]
+},
+{
+  "rktr_slot_id": "ad_rect_atf_01",
+  "rktr_ad_id": "CNNi/entertainment/leaf/bst",
+  "sizes": [[1,2],[300,250],[300,600],[300,850],[300,1050]],
+  "hasInViewRefresh": "false",
+  "inViewRefreshCount": "0",  
+  "targeting": [["pos",["rect_atf_01"]]],
+  "responsive":[[['1024', '0'], [['300', '1050'], ['300', '850'], ['300', '600'], ['300', '250'], ['1', '2']]], [['782', '0'], [['300', '850'], ['300', '600'], ['300', '250'], ['1', '2']]], [['0', '0'], ['suppress']]]
+},
+{
+  "rktr_slot_id": "ad_rect_atf_02",
+  "rktr_ad_id": "CNNi/entertainment/leaf/bst",
+  "sizes": [[1,2],[300,250]],
+  "hasInViewRefresh": "false",
+  "inViewRefreshCount": "0",  
+  "targeting": [["pos",["rect_atf_02"]]],
+  "responsive":[[['1024', '0'], ['suppress']], [['782', '0'], ['suppress']], [['0', '0'], [['300', '250'], ['1', '2']]]]
+},
+{
+  "rktr_slot_id": "ad_rect_btf_01",
+  "rktr_ad_id": "CNNi/entertainment/leaf/bst",
+  "sizes": [[1,2],[300,250],[300,251],[300,600],[300,601]],
+  "hasInViewRefresh": "false",
+  "inViewRefreshCount": "0",  
+  "targeting": [["pos",["rect_btf_01"]]],
+  "responsive":[[['1024', '0'], [['300', '601'], ['300', '600'], ['300', '251'], ['300', '250'], ['1', '2']]], [['782', '0'], [['300', '601'], ['300', '600'], ['300', '251'], ['300', '250'], ['1', '2']]], [['0', '0'], [['300', '251'], ['300', '250'], ['1', '2']]]]
+},
+{
+  "rktr_slot_id": "ad_rect_btf_02",
+  "rktr_ad_id": "CNNi/entertainment/leaf/bst",
+  "sizes": [[1,2],[300,250]],
+  "hasInViewRefresh": "false",
+  "inViewRefreshCount": "0",  
+  "targeting": [["pos",["rect_btf_02"]]],
+  "responsive":[[['1024', '0'], ['suppress']], [['782', '0'], ['suppress']], [['0', '0'], [['300', '250'], ['1', '2']]]]
+},
+{
+  "rktr_slot_id": "ad_rect_btf_03",
+  "rktr_ad_id": "CNNi/entertainment/leaf/bst",
+  "sizes": [[1,2],[300,250]],
+  "hasInViewRefresh": "false",
+  "inViewRefreshCount": "0",  
+  "targeting": [["pos",["rect_btf_03"]]],
+  "responsive":[[['1024', '0'], ['suppress']], [['782', '0'], ['suppress']], [['0', '0'], [['300', '250'], ['1', '2']]]]
+},
+{
+  "rktr_slot_id": "ad_bnr_btf_01",
+  "rktr_ad_id": "CNNi/entertainment/leaf/bst",
+  "sizes": [[1,2],[728,90],[728,91],[970,66],[970,90],[970,91],[970,250]],
+  "hasInViewRefresh": "false",
+  "inViewRefreshCount": "0",  
+  "targeting": [["pos",["bnr_btf_01"]]],
+  "responsive":[[['1024', '0'], [['970', '250'], ['970', '91'], ['970', '90'], ['970', '66'], ['728', '91'], ['728', '90'], ['1', '2']]], [['782', '0'], [['728', '91'], ['728', '90'], ['1', '2']]], [['0', '0'], ['suppress']]]
+},
+{
+  "rktr_slot_id": "ad_oop_skin_01",
+  "rktr_ad_id": "CNNi/entertainment/leaf/bst",
+  "sizes": [[1,1],[1,2]],
+  "hasInViewRefresh": "false",
+  "inViewRefreshCount": "0",  
+  "targeting": [["pos",["oop_skin_01"]]],
+  "responsive":[[['1024', '0'], [['1', '2'], ['1', '1']]], [['782', '0'], ['suppress']], [['0', '0'], ['suppress']]]
+},
+{
+  "rktr_slot_id": "ad_oop_float_01",
+  "rktr_ad_id": "CNNi/entertainment/leaf/bst",
+  "sizes": [[1,1],[1,2]],
+  "hasInViewRefresh": "false",
+  "inViewRefreshCount": "0",  
+  "targeting": [["pos",["oop_float_01"]]],
+  "responsive":[[['1024', '0'], [['1', '2'], ['1', '1']]], [['782', '0'], [['1', '2'], ['1', '1']]], [['0', '0'], [['1', '2'], ['1', '1']]]]
+},
+{
+  "rktr_slot_id": "ad_oop_float_02",
+  "rktr_ad_id": "CNNi/entertainment/leaf/bst",
+  "sizes": [[1,1],[1,2]],
+  "hasInViewRefresh": "false",
+  "inViewRefreshCount": "0",  
+  "targeting": [["pos",["oop_float_02"]]],
+  "responsive":[[['1024', '0'], ['suppress']], [['782', '0'], ['suppress']], [['0', '0'], [['1', '2'], ['1', '1']]]]
+},
+{
+  "rktr_slot_id": "ad_nat_btf_01",
+  "rktr_ad_id": "CNNi/entertainment/leaf/bst",
+  "sizes": [[1,2]],
+  "hasInViewRefresh": "false",
+  "inViewRefreshCount": "0",  
+  "targeting": [["pos",["nat_btf_01"]], ["strnativekey",["UEVAb5wr61SGDopcwzr4aCSN"]]],
+  "responsive":[[['1024', '0'], [['1', '2']]], [['782', '0'], [['1', '2']]], [['0', '0'], [['1', '2']]]]
+}
+]);
